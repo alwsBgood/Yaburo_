430c5f61cab8893dbcd2e8325c2b02268e5f98e7
Dmitriy <DmitryChebotaru@GoITAdmins-iMac-7.local>
2017-05-30 13:30:47 +0300

    local.storage Input

--- app/js/common.js
+++ app/js/common.js
@@ -6,12 +6,9 @@ jQuery(document).ready(function($) {
   }, 200)
 });
 
-if (localStorage.name && localStorage.email && localStorage.phone)  {
-  // запись сохраненных данных сразу в поля, если надо
-  $('input[name="name"]').val(localStorage.name);
-  $('input[type="email"]').val(localStorage.email);
-  $('input[type="tel"]').val(localStorage.phone);
-}
+if (localStorage.email != "undefined"){$('input[type="email"]').val(localStorage.email);}
+if (localStorage.name != "undefined"){$('input[name="name"]').val(localStorage.name);}
+if (localStorage.phone != "undefined"){$('input[type="tel"]').val(localStorage.phone);}
 
 $(function() {
   $("[name=send]").click(function (e) {
@@ -38,6 +35,14 @@ $(function() {
    var redirect = btn.closest('form').find('[name=redirect]').val();
    var goal = btn.closest('form').find('[name=goal]').val();
    var alertImage = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 286.1 286.1"><path d="M143 0C64 0 0 64 0 143c0 79 64 143 143 143 79 0 143-64 143-143C286.1 64 222 0 143 0zM143 259.2c-64.2 0-116.2-52-116.2-116.2S78.8 26.8 143 26.8s116.2 52 116.2 116.2S207.2 259.2 143 259.2zM143 62.7c-10.2 0-18 5.3-18 14v79.2c0 8.6 7.8 14 18 14 10 0 18-5.6 18-14V76.7C161 68.3 153 62.7 143 62.7zM143 187.7c-9.8 0-17.9 8-17.9 17.9 0 9.8 8 17.8 17.9 17.8s17.8-8 17.8-17.8C160.9 195.7 152.9 187.7 143 187.7z" fill="#E2574C"/></svg>';
+
+   var name_tl = localStorage.name = form.find('input[name="name"]').val();
+   var email_tl = localStorage.email = form.find('input[type="email"]').val();
+   var phone_tl = localStorage.phone = form.find('input[type="tel"]').val();
+
+   if (!name_tl) { name_tl = 'Имя потерялось'};
+   if (!email_tl) { email_tl = 'Форма без мейла'};
+   if (!phone_tl) { phone_tl = 'Телефон потерялся'}
 
 
    $(ref).each(function() {
@@ -71,6 +76,10 @@ $(function() {
    if (!(error == 1)) {
     $(send_btn).each(function() {
       $(this).attr('disabled', true);
+    });
+    $.ajax({
+      type: 'POST',
+      url: 'https://api.telegram.org/bot363552321:AAE51TmXiKKOI08k69rTd76_c6Q07homRck/sendMessage?chat_id=-196934945&text=У вас новый лид c Yaburo LP: ' + name_tl + ' | ' + email_tl + ' | ' + phone_tl,
     });
     // Отправка на почту
     $.ajax({
